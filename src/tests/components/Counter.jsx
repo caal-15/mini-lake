9955b77a577da43aec5a120b86b85d85ca4f1509
Carlos Gonzalez <caal.0522@gmail.com>
2019-01-21 14:10:47 -0500

    New Version!
    - Adds webpack config.
    - Updates build.

--- src/tests/components/Counter.jsx
+++ src/tests/components/Counter.jsx
@@ -4,18 +4,19 @@ const Counter = ({ counter, addToCounter, setCounter }) => {
   return (
     <div>
       <h1>{counter}</h1>
-      <button className='add' onClick={addToCounter}>Add</button>
-      {setCounter
-        ? <button
-          className='set'
+      <button className="add" onClick={addToCounter}>
+        Add
+      </button>
+      {setCounter ? (
+        <button
+          className="set"
           onClick={() => {
             setCounter(100)
           }}
         >
           Set to 100
         </button>
-        : null
-      }
+      ) : null}
     </div>
   )
 }
